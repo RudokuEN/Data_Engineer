14e12b1d5cecb9aca6266dee1572e39bb5f90717
Anrew <andrey.grimm16@gmail.com>
2025-01-05 14:50:24 +0300

    Add comment

--- Final_Task.py
+++ Final_Task.py
@@ -2,12 +2,13 @@ data = [
     {"item": "apple", "category": "fruit", "price": 1.2, "quantity": 10},
     {"item": "banana", "category": "fruit", "price": 0.5, "quantity": 5},
     {"item": "milk", "category": "dairy", "price": 1.5, "quantity": 2},
-    {"item": "bread", "category": "bakery", "price": 2.0, "quantity": 15}]
+    {"item": "bread", "category": "bakery", "price": 2.0, "quantity": 3}]
 
 def total_revenue(purchases):
     """
-    :param purchases:
-    :return: total revenue
+    :param purchases: Входные данные (data)
+    :return: Общая выручка (цена * количество для всех записей).
+    Вывод: Общая выручка: 23.5
     """
     result = []
     for product in purchases:
@@ -16,8 +17,9 @@ def total_revenue(purchases):
 
 def items_by_category(purchases):
     """
-    :param purchases:
-    :return: result
+    :param purchases: Входные данные (data)
+    :return: Словарь -> {Категории: Список уникальных товаров в этой категории}
+    Вывод: Товары по категориям: {'fruit': ['apple', 'banana'], 'dairy': ['milk'], 'bakery': ['bread']}
     """
     result = {}
     for p_data in purchases:
@@ -29,46 +31,62 @@ def items_by_category(purchases):
 
 def expensive_purchases(purchases, min_price):
     """
-    :param purchases:
-    :param min_price:
-    :return:
+    :param purchases: Входные данные (data)
+    :param min_price: Минимальная цена
+    :return: Все покупки, где цена товара больше или равна min_price
+    Вывод, если min_price = 1.0:
+    Покупки дороже 1.0:
+    [{'item': 'apple', 'category': 'fruit', 'price': 1.2, 'quantity': 10},
+    {'item': 'milk', 'category': 'dairy', 'price': 1.5, 'quantity': 2},
+    {'item': 'bread', 'category': 'bakery', 'price': 2.0, 'quantity': 15}]
     """
     result = []
-    for d_data in purchases:
-        if d_data['price'] >= min_price:
-            result.append(d_data)
+    for part_data in purchases:
+        if part_data['price'] >= min_price:
+            result.append(part_data)
     return f'Покупки дороже {min_price}: {result}'
 
 def average_price_by_category(purchases):
+    """
+    :param purchases: Входные данные (data)
+    :return: Средняя цена товаров по каждой категории
+    Вывод: Средняя цена по категориям: {'fruit': 0.85, 'dairy': 1.5, 'bakery': 2.0}
+    """
     result = {}
-    for d_data in purchases:
-        if d_data['category'] not in result:
-            result[d_data['category']] = [d_data['price']]
+    for part_data in purchases:
+        if part_data['category'] not in result:
+            result[part_data['category']] = [part_data['price']]
         else:
-            result [d_data['category']].append(d_data['price'])
-    for d_2_data in result:
-        result [d_2_data] = sum(result[d_2_data]) / len(result[d_2_data])
+            result[part_data['category']].append(part_data['price'])
+    for part_2_data in result:
+        result [part_2_data] = sum(result[part_2_data]) / len(result[part_2_data])
 
     return f'Средняя цена по категориям: {result}'
 
 def most_frequent_category(purchases):
-    result = {}
-    for d_data in purchases:
-        if d_data['category'] not in result:
-            result[d_data['category']] = [d_data['quantity']]
+    """
+    :param purchases: Входные данные (data)
+    :return: Категории, в которой куплено больше всего единиц товаров (учитывайте поле quantity)
+    Вывод:
+    Категория с наибольшим количеством проданных товаров: fruit
+    """
+    result_part_one = {}
+    for part_data in purchases:
+        if part_data['category'] not in result_part_one:
+            result_part_one[part_data['category']] = [part_data['quantity']]
         else:
-            result[d_data['category']].append(d_data['quantity'])
-    for d_d_data in result:
-        result[d_d_data] = sum(result[d_d_data])
-    max_quantity = max(result.values())
-    answer = []
-    for d_d_data in result:
-        if result[d_d_data] == max_quantity:
-            answer.append(d_d_data)
-    if len(answer) > 1:
-        return f'Категории с наибольшим количеством проданных товаров: {", ".join(answer)}'
+            result_part_one[part_data['category']].append(part_data['quantity'])
+    for part_2_data in result_part_one:
+        result_part_one[part_2_data] = sum(result_part_one[part_2_data])
+    max_quantity = max(result_part_one.values())
+    result_part_two = []
+    for part_3_data in result_part_one:
+        if result_part_one[part_3_data] == max_quantity:
+            result_part_two.append(part_3_data)
+    if len(result_part_two) > 1:
+        return f'Категории с наибольшим количеством проданных товаров: {", ".join(result_part_two)}'
     else:
-        return f'Категория с наибольшим количеством проданных товаров: {answer[0]}'
+        return f'Категория с наибольшим количеством проданных товаров: {result_part_two[0]}'
 
 print(total_revenue(data))
 print(items_by_category(data))
